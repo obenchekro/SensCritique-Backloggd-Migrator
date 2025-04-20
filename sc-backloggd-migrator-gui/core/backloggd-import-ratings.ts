e282913cad09c790c33550f346da5c3a8bfd2095
obenchekro <othmanebenchekroun@outlook.fr>
2025-04-21 00:13:45 +0200

    feat: redirection to the rated games page as a final electron page

--- sc-backloggd-migrator-gui/core/backloggd-import-ratings.ts
+++ sc-backloggd-migrator-gui/core/backloggd-import-ratings.ts
@@ -2,11 +2,14 @@ import { BrowserWindow } from "electron";
 import { slugify } from "../../sc-backloggd-migrator-utils/slug";
 import { readSavedGames } from "../../sc-backloggd-migrator-utils/filesystem";
 import { delay } from '../../sc-backloggd-migrator-utils/delay';
-const { BACKLOGGD_AUTOMATION_SCRIPT } = require('../../sc-backloggd-migrator-utils/dom');
+const { BACKLOGGD_AUTOMATION_SCRIPT, USERNAME_BACKLOGGD_DOM_CONTENT } = require('../../sc-backloggd-migrator-utils/dom');
 
 export async function runBackloggdRatingAutomation(window: BrowserWindow): Promise<void> {
   try {
     const collectionGameRatings = readSavedGames();
+    const username = await window.webContents.executeJavaScript(`
+      (${USERNAME_BACKLOGGD_DOM_CONTENT.toString()})()
+    `);
 
     for (const game of collectionGameRatings) {
       await delay(3000)
@@ -18,6 +21,9 @@ export async function runBackloggdRatingAutomation(window: BrowserWindow): Promi
       await window.webContents.executeJavaScript(`(${BACKLOGGD_AUTOMATION_SCRIPT.toString()})()(${game.rating})`)
         .catch(error => console.error(`Failed to rate: ${game.title}. See the stacktrace here: ${error}`));
     }
+
+    const gameUrl = `https://backloggd.com/u/${username}/games/`;
+    await window.loadURL(gameUrl);
   } catch (error) {
     console.error(`Rating automation failed. See the stacktrace here: ${error}`);
   }
--- sc-backloggd-migrator-utils/dom.js
+++ sc-backloggd-migrator-utils/dom.js
@@ -20,33 +20,43 @@ const USERNAME_SENSCRITIQUE_DOM_CONTENT = async () => {
     });
 };
 
+const USERNAME_BACKLOGGD_DOM_CONTENT = async () => {
+    const profileLink = document.querySelector('a[href^="/u/"]');
+    if (profileLink) {
+        const href = profileLink.getAttribute("href");
+        const username = href.split("/u/")[1].replace("/", "");
+        return username;
+    }
+}
+
 const BACKLOGGD_AUTOMATION_SCRIPT = () => async (rating) => {
     const gameIdEl = document.querySelector('#game-page-id');
     const gameId = gameIdEl?.getAttribute('game_id');
     if (!gameId) console.warn("Cannot retrieve gameId of the current game page.");
-  
+
     const csrfMeta = document.querySelector('meta[name="csrf-token"]');
     const csrf = csrfMeta?.content;
     if (!csrf) console.warn("Cannot retrieve CSRF token to ensure the reliability of the request.")
-      
+
     const response = await fetch(`/rate/${gameId}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "X-CSRF-Token": csrf,
-        "X-Requested-With": "XMLHttpRequest",
-      },
-      body: `rating=${rating}`
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "X-CSRF-Token": csrf,
+            "X-Requested-With": "XMLHttpRequest",
+        },
+        body: `rating=${rating}`
     });
-  
+
     if (response.ok) {
-      console.log(`Succesfully rated the game ${gameId} with the following rating: ${rating}`);
+        console.log(`Succesfully rated the game ${gameId} with the following rating: ${rating}`);
     } else {
-      console.warn(`Request to the Backloggd server failed with the following status code: ${response.status}`);
+        console.warn(`Request to the Backloggd server failed with the following status code: ${response.status}`);
     }
-  };
+};
 
 module.exports = {
     USERNAME_SENSCRITIQUE_DOM_CONTENT,
+    USERNAME_BACKLOGGD_DOM_CONTENT,
     BACKLOGGD_AUTOMATION_SCRIPT
 };
