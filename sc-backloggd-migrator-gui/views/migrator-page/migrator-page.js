8cf8956ce77ea21536e04a3c2b943333620c3320
obenchekro <othmanebenchekroun@outlook.fr>
2025-12-06 03:20:13 +0100

    chore(views): better syntax

--- sc-backloggd-migrator-gui/views/migrator-page/migrator-page.js
+++ sc-backloggd-migrator-gui/views/migrator-page/migrator-page.js
@@ -1,6 +1,4 @@
 window.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("start-btn");
-    button?.addEventListener("click", () => {
-    window.electronAPI?.startMigration?.();
-    });
+    button?.addEventListener("click", () => window.electronAPI?.startMigration?.());
 });
